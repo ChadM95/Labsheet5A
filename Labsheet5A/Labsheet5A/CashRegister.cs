24b82aa21ace1b55a30d9d8b08a68f20ef826fd7
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-05 20:48:56 +0000

    create CashRegister class

---
+++ Labsheet5A/Labsheet5A/CashRegister.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TestCashRegister
+{
+	public class CashRegister
+	{
+		//Properties
+		public double Total { get; set; }
+		public int Items { get; set; }
+
+
+
+		//Constructor
+		public CashRegister()
+		{
+		}
+
+        //Methods
+        public override string ToString()
+        {
+            return $"";
+        }
+
+		public void AddItem(double price)
+		{
+			Total = Total + price;
+			Items++;
+			Console.WriteLine($"Adding an item worth{price} to ");
+		}
+    }
+}
+
